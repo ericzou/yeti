e47d6043b07171b3e687f0a059e4f93235e77b5d
Eric Zou <ezou@carwoo.com>
2011-02-05 19:33:02 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yeti::Application.config.secret_token = 'ff6983d2482ce93937bc57ac5d7f7bedd0b92e6c4f35d406276dcac2b64e5c5e57a641382081bd5553b969898f7dec95b4dd1539d5c0649b62ef99ce116d0d8c'
