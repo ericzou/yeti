51da37b0b442a1895968efae4dfb9f53a182aac1
Eric Zou <ezou@carwoo.com>
2011-02-21 22:35:49 -0800

    wi

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -1,5 +1,8 @@
 class LandingsController < ApplicationController
   def welcome
+    if current_user
+      redirect_to home_user_path(current_user) and return 
+    end
     @user = User.new
   end
 
