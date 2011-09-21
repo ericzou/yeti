593852882935d887c6ff026ea15949ab716b0510
Eric Zou <ezou@carwoo.com>
2011-09-21 16:19:05 -0700

    fixed the email link

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ Yeti::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => "example.com" }
+  config.action_mailer.default_url_options = { :host => "marvellist.com" }
   
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
